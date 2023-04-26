e05631c0d86e101ba09cb62160fec0d39cde4c1c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-04-26 09:25:50 +0100

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+import { ScrollViewModule } from '@progress/kendo-angular-scrollview';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageGalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollViewModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
